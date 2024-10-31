c9907033fec52163d234b82a64362fea51dded01
erkin7333 <erkinnajimov90@gmail.com>
2024-10-31 20:39:46 +0500

    sql

--- account/models.py
+++ account/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_director = models.BooleanField(default=False, verbose_name="Director")
+    is_archivist = models.BooleanField(default=False, verbose_name="Arxivchi")
+    is_download = models.BooleanField(default=False, verbose_name="Download")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Ro'yxatdan o'tgan vaqti")
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = "Foydalanuvchi"
+        verbose_name_plural = "Foydalanuvchilar"
--- config/settings.py
+++ config/settings.py
@@ -131,7 +131,7 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-#AUTH_USER_MODEL = 'account.User'
+AUTH_USER_MODEL = 'account.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
