28be4e75da380055aacb401e814448334629d91e
erkin7333 <erkinnajimov90@gmail.com>
2024-11-01 15:42:50 +0500

    End View

--- main/views.py
+++ main/views.py
@@ -1,8 +1,12 @@
+from array import array
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import MultiPartParser, FormParser
+from yaml import serialize
+
 from .models import Category, Folders, DocumentType, Files
 from django.db.models import Q
 from .serializers import (CategorySerializer, FoldersSerializer, DocumentTypeSerializer,
@@ -113,3 +117,47 @@ class AddFilesAPIView(APIView):
                             status=status.HTTP_201_CREATED)
         return Response({'message': "Malumot yuklashda xatolik yuzberdi.....", 'data': serializer.errors},
                         status=status.HTTP_400_BAD_REQUEST)
+
+
+class SearchFilesAPIView(APIView):
+    """
+    Malumotlarni Papka id va fayllari kodi bilan izlash uchun views
+    """
+    permission_classes = [IsAuthenticated]
+    def get(self, request, *args, **kwargs):
+        folder_id = request.query_params.get('folder_id', None)
+        file_code = request.query_params.get('file_code', None)
+        if not folder_id:
+            return Response({"message": "Folder ID va Document ID kiritilishi shart"}, status=status.HTTP_400_BAD_REQUEST)
+        files = Files.objects.filter(folder_id=folder_id)
+        if file_code:
+            files = files.filter(Q(file_code__icontains=files))
+        if files.exists():
+            serializer = FilesSerializer(files, many=True)
+            return Response({'message': "Siz izlagan malumot....", 'data': serializer.data},
+                            status=status.HTTP_200_OK)
+        else:
+            return Response({'message': "Hechqanday malumot topilamdi.."}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SearchFolderAPIView(APIView):
+    """
+    Kategoriyaga tegishli Papkalarni izlash uchun views
+    """
+    permission_classes = [IsAuthenticated]
+    def get(self, request, *args, **kwargs):
+        category_id = request.query_params.get('category_id')
+        number = request.query_params.get('number', '')
+        name = request.query_params.get('name', '')
+
+        if not category_id:
+            return Response({'message': "Bu id da malumot topilmadi......."}, status=status.HTTP_400_BAD_REQUEST)
+        folders = Folders.objects.filter(category_id=category_id)
+        if number or name:
+            folders = folders.filter(Q(number__icontains=number) | Q(name__icontains=name))
+        if folders.exists():
+            serializer = FoldersSerializer(folders, many=True)
+            return Response({'message': "Siz izlagan malumot......", 'data': serializer.data},
+                            status=status.HTTP_200_OK)
+        else:
+            return Response({'message': "Siz izlagan malumot topilmadi ....."}, status=status.HTTP_404_NOT_FOUND)
