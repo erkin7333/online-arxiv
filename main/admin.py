772d8e8cf4c7bd5cacd1ae10e83cf456ff1e9dce
erkin7333 <erkinnajimov90@gmail.com>
2024-11-02 12:12:58 +0500

    Admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Category, Folders, DocumentType, Files
 
-# Register your models here.
+
+class FoldersInline(admin.TabularInline):
+    model = Folders
+    extra = 1
+    fields = ['id', 'name', 'number', 'doc_type', 'created_at']
+    readonly_fields = ['created_at']
+    can_delete = True
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [FoldersInline]
+    list_display = ['id', 'name', 'created_at']
+    list_display_links = ['id', 'name']
+    search_fields = ['name']
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class DocumentTypeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'created_at']
+    list_display_links = ['id', 'name']
+    search_fields = ['name']
+
+admin.site.register(DocumentType, DocumentTypeAdmin)
+
+admin.site.register(Files)
